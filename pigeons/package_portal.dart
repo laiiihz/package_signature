e9571532ba1feb69490529a0c38d9558cd798ce1
laiiihz <laiiihz@outlook.com>
2026-01-13 09:44:09 +0800

    Complete the missing package in Kotlin

--- pigeons/package_portal.dart
+++ pigeons/package_portal.dart
@@ -4,6 +4,9 @@ import 'package:pigeon/pigeon.dart';
   dartOut: 'lib/src/package_portal.g.dart',
   kotlinOut:
       'android/src/main/kotlin/tech/laihz/package_signature/package_portal.g.kt',
+  kotlinOptions: KotlinOptions(
+    package: 'tech.laihz.package_signature',
+  ),
 ))
 @HostApi()
 abstract class PackagePortal {
